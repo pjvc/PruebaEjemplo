29faf54b31c85a5ef72c6d6811b71d8465e22fe6
me <me@DESKTOP-213L5JE>
2023-09-19 08:36:59 -0500

    Agregar archivos de proyecto.

---
+++ PruebaEjemplo/Controllers/ElementoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PruebaEjemplo.Controllers
+{
+    public class ElementoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PruebaEjemplo/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace PruebaEjemplo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
